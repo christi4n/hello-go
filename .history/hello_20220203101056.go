c21c5c724d4c525d119e8bea381d0e8bf0b37c21
christi4n <christian@mydevfactory.net>
2022-02-03 10:13:45 +0100

    [TASK] Add gin dependency and basic server

---
+++ .history/hello_20220203101056.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	// Create a router object
+	r := gin.Default()
+
+	// Return a status 200 code and a string hello world
+	// for the "/hello" path
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	// Listen on port 3000
+	r.Run(":8181")
+}
